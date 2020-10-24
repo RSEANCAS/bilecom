5dabfd724eefc5c5f9db092fe77c640c849f9295
Usuario01 <antony.lazaroyepez@gmail.com>
2020-10-24 02:19:02 -0500

    bl - SedeBe

--- bilecom.be/SedeBe.cs
+++ bilecom.be/SedeBe.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace bilecom.be
 {
-    class SedeBe
+    public class SedeBe
     {
+        public int SedeId { get; set; }
+        public string Ruc { get; set; }
+        public int Rubro { get; set; }
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Ubigeo { get; set; }
+        public string CreadoPor { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string ModificadoPor { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        
     }
 }
