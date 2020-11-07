ba4a593ebea3e44628e07151517f55c0fab87b28
luismendoza <luis.mtorres93@gmail.com>
2020-11-06 23:01:55 -0500

    LM Avance 2 Login

---
+++ backend/bilecom.app/Assets/js/pages/login.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#frm-login").bootstrapValidator({
+        fields: {
+            "txt-ruc": {
+                validators: {
+                    notEmpty: {
+                        message: "Debe ingresar el Ruc"
+                    }
+                }
+            },
+            "txt-usuario": {
+                validators: {
+                    notEmpty: {
+                        message: "Debe ingresar el Usuario"
+                    }
+                }
+            },
+            "txt-contraseña": {
+                validators: {
+                    notEmpty: {
+                        message: "Ingresar contraseña correta"
+                    }
+                }
+            },
+        } 
+    });
+});
