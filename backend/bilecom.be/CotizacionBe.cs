5f793ea0a7d99b8ca79c9723ddcc9bcfc5012eaa
luismendoza <luis.mtorres93@gmail.com>
2020-11-21 01:34:42 -0500

    LM - Cambios temporales

---
+++ backend/bilecom.be/CotizacionBe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilecom.be
+{
+    public class CotizacionBe:Base
+    {
+        public int CotizacionId { get; set; }
+        public int EmpresaId { get; set; }
+        public DateTime FechaHoraEmision { get; set; }
+        public int SerieId { get; set; }
+        public int NroComprobante { get; set; }
+        public string NroPedido { get; set; }
+        public int ClienteId { get; set; }
+        
+        public int PersonalId { get; set; }
+        public PersonalBe Personal { get; set; }
+        public int MonedaId { get; set; }
+        public float TotalImporte { get; set; }
+        public bool FlagEliminado { get; set; }
+        public string CreadoPor { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string ModificadoPor { get; set; }
+        public DateTime FechaModificacion { get; set; }
+    }
+}
---
+++ backend/bilecom.da/CotizacionDa.cs
@@ -0,0 +1,49 @@
+﻿using bilecom.be;
+using bilecom.ut;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilecom.da
+{
+    class CotizacionDa
+    {
+        public List<CotizacionBe> fListar(SqlConnection cn, int EmpresaId, string NombresCompletosPersonal, string RazonSocial, 
+            DateTime FechaHoraEmisionDesde, DateTime FechaHoraEmisionHasta)
+        {
+            List<CotizacionBe> lCotizacion = null;
+            using (SqlCommand cmd = new SqlCommand("usp_cotizacion_listar", cn))
+            {
+                // Instanciando a la funcion CommandType
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@empresaId", EmpresaId);
+                cmd.Parameters.AddWithValue("@nombresPersonal", NombresCompletosPersonal);
+                cmd.Parameters.AddWithValue("@cliente", RazonSocial);
+                cmd.Parameters.AddWithValue("@fechaHoraEmisionDesde", FechaHoraEmisionDesde);
+                cmd.Parameters.AddWithValue("@fechaHoraEmisionHasta", FechaHoraEmisionHasta);
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    if (dr.HasRows)
+                    {
+                        lCotizacion = new List<CotizacionBe>();
+
+                        while (dr.Read())
+                        {
+                            CotizacionBe item = new CotizacionBe();
+                            item.Fila = dr.GetData<int>("Fila");
+                            item.SerieId = dr.GetData<int>("SerieId");
+                            item.NroComprobante = dr.GetData<int>("NroComprobante");
+
+                            lCotizacion.Add(item);
+                        }
+                    }
+                }
+            }
+            return lCotizacion;
+        }
+    }
+}
