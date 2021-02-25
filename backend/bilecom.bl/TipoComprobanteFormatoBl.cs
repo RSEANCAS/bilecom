415f6e5576de288f15ead22986fe27df37d1ea07
RSEANCAS <ronald.jsb@hotmail.com>
2021-02-25 03:30:07 -0500

    RS - TipoComprobanteFormato

---
+++ backend/bilecom.bl/TipoComprobanteFormatoBl.cs
@@ -0,0 +1,30 @@
+﻿using bilecom.be;
+using bilecom.da;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilecom.bl
+{
+    public class TipoComprobanteFormatoBl : Conexion
+    {
+        TipoComprobanteFormatoDa tipoComprobanteFormatoDa = new TipoComprobanteFormatoDa();
+
+        public List<TipoComprobanteFormatoBe> ListarTipoComprobanteFormatoPorTipoComprobante(int tipoComprobanteId)
+        {
+            List<TipoComprobanteFormatoBe> lista = null;
+            try
+            {
+                cn.Open();
+                lista = tipoComprobanteFormatoDa.ListarPorTipoComprobante(tipoComprobanteId, cn);
+                cn.Close();
+            }
+            catch (Exception ex) { lista = null; }
+            finally { if (cn.State == ConnectionState.Open) cn.Close(); }
+            return lista;
+        }
+    }
+}
