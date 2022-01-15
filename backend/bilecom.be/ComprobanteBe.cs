d82719a65e563ff31a3e95d550bb0d45625a65ef
Usuario01 <antony.lazaroyepez@gmail.com>
2022-01-14 22:34:50 -0500

    BL - CAmbios

---
+++ backend/bilecom.be/ComprobanteBe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilecom.be
+{
+    public class ComprobanteBe
+    {
+        public EmpresaBe Empresa { get; set; }
+        public SerieBe Serie { get; set; }
+        public int NroComprobante { get; set; }
+        public DateTime FechaHoraEmision { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public MonedaBe Moneda { get; set; }
+        public TipoOperacionVentaBe TipoOperacionVenta { get; set; }
+        public ClienteBe Cliente { get; set; }
+        public FormaPagoBe FormaPago { get; set; }
+        public decimal TotalIgv { get; set; }
+        public decimal ImporteTotal { get; set; }
+        public bool FlagAnulado { get; set; }
+
+    }
+}
---
+++ backend/bilecom.da/ReporteDa.cs
@@ -0,0 +1,46 @@
+﻿using bilecom.be;
+using bilecom.ut;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilecom.da
+{
+    public class ReporteDa
+    {
+        public List<ComprobanteBe> ComprobanteListar(SqlConnection cn, int empresaId, int sedeId, string fechaInicio, string fechaFin, string cat01_id, string serie_id, string numero, string razonSocialComprador, string usuario_id)
+        {
+            List<ComprobanteBe> lista = new List<ComprobanteBe>();
+            ComprobanteBe item;
+            using (SqlCommand cmd = new SqlCommand("dbo.usp_reporte_ventas_listar", cn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpresaId", empresaId.GetNullable());
+                cmd.Parameters.AddWithValue("@SedeId", sedeId.GetNullable());
+                cmd.Parameters.AddWithValue("@FechaInicio", fechaInicio.GetNullable());
+                cmd.Parameters.AddWithValue("@fechaFin", fechaFin.GetNullable());
+                cmd.Parameters.AddWithValue("@SerieId", serie_id.GetNullable());
+                cmd.Parameters.AddWithValue("@numero", numero.GetNullable());
+                cmd.Parameters.AddWithValue("@razonSocialComprador", razonSocialComprador.GetNullable());
+                cmd.Parameters.AddWithValue("@usuario_id", usuario_id.GetNullable());
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            item = new ComprobanteBe();
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+}
